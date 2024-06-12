7741474d835ebcabb4c63a636959276868961146
riteshPatil <ritesh93patil@outlook.com>
2024-06-12 13:40:11 +0530

    kafka Wikimedia Project initial commit

---
+++ src/test/java/com/ritesh/kafka_wikimedia_project/KafkaWikimediaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritesh.kafka_wikimedia_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWikimediaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
